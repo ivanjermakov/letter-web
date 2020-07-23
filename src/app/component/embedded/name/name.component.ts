ab87a97899a594caa82cb81f02a2be95583e9fbb
ivanjermakov <ivanjermakov1@gmail.com>
2020-07-23 14:01:27 +0300

    #18 features view

--- src/app/component/embedded/name/name.component.ts
+++ src/app/component/embedded/name/name.component.ts
@@ -1,15 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-name',
-  templateUrl: './name.component.html',
-  styleUrls: ['./name.component.sass']
+	selector: 'app-name',
+	templateUrl: './name.component.html',
+	styleUrls: ['./name.component.sass']
 })
 export class NameComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
